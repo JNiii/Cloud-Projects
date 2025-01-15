705ea1279cc68dd020c80c4bb58158d4b871305d
JNiii <43835501+JNiii@users.noreply.github.com>
2025-01-15 09:51:06 +0800

    Adding Microsoft WhatTheHack Networking Files
    Added project files for WhatTheHack Networking challenge by Microsoft.
    Added ARM Templates for each deployment.

---
+++ WhatTheHack-Networking/New-AzResourceGroupDeployment.ps1
@@ -0,0 +1,11 @@
+$rg = Get-AzResourceGroup -location eastus
+$vnet = "./VirtualNetwork.json"
+$nsg = "./NetworkSecurityGroups.json"
+$vnetgw = "./VNetGateway.json"
+$vnetpeer = "./VNetPeering.json"
+
+New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $nsg
+New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnet
+
+#New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnetgw -AsJob
+#New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnetpeer -AsJob
