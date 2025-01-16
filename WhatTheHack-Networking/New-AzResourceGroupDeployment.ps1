2954cdf307155e1f2fa14c804d1a8698888ff048
JNiii <ineojay@gmail.com>
2025-01-16 11:09:12 +0800

    Added JSON and script files

--- WhatTheHack-Networking/New-AzResourceGroupDeployment.ps1
+++ WhatTheHack-Networking/New-AzResourceGroupDeployment.ps1
@@ -3,9 +3,11 @@ $vnet = "./VirtualNetwork.json"
 $nsg = "./NetworkSecurityGroups.json"
 $vnetgw = "./VNetGateway.json"
 $vnetpeer = "./VNetPeering.json"
+$vm = "./VirtualMachines.json"
 
-New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $nsg
-New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnet
+#New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $nsg
+#New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnet
+New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vm
 
 #New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnetgw -AsJob
 #New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnetpeer -AsJob
