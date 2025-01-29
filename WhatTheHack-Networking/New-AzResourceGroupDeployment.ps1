2cc2631c3096eaeb1cd35fa82ddf86dd23761f7b
JNiii <ineojay@gmail.com>
2025-01-29 14:05:47 +0800

    Edited Firewall JSON. Cleaned up Route Tables JSON. Modified VirtualNetwork JSON to include Route Tables and NSG upon VNet creation.

--- WhatTheHack-Networking/New-AzResourceGroupDeployment.ps1
+++ WhatTheHack-Networking/New-AzResourceGroupDeployment.ps1
@@ -13,10 +13,11 @@ $scriptURL = read-host -prompt "Enter IIS SAS URL"
 $firewallName = read-host -prompt "Enter Firewall Name"
 
 New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $nsg
+New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $rt
 New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnet
 New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vm -vaultName $vaultname -scriptURL $scriptURL -AsJob
+New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $fw -firewallName $firewallName -asJob
 New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnetgw
 New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $gwconn -vaultName $vaultname
 New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $vnetpeer
-New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $fw -firewallName $firewallName
-#New-AzResourceGroupDeployment -resourceGroup $rg.resourceGroupName -templatefile $rt -firewallName $firewallName
+
