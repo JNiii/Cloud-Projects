7898e0b162af5c30beac117829d03042e5416f28
JNiii <ineojay@gmail.com>
2025-02-14 19:59:05 +0800

    Added comments on IIS.ps1 file

--- Azure-Networking-01/Powershell_Scripts/IIS.ps1
+++ Azure-Networking-01/Powershell_Scripts/IIS.ps1
@@ -1,4 +1,6 @@
 import-module servermanager
 add-windowsfeature web-server -includeallsubfeature
 set-content -path "C:\inetpub\wwwroot\Default.html" -Value "This is the server $($env:computername)!"
+
+#Optional if you want to enable ICMP or Ping requests
 #Enable-NetFirewallRule -name FPS-ICMP4-ERQ-In 
