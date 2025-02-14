9d3dd2d20817de805ce690b12b5fed96cdfd638b
JNiii <ineojay@gmail.com>
2025-02-14 19:54:16 +0800

    Renamed powershell scripts folder

---
+++ Azure-Networking-01/Powershell_Scripts/IIS.ps1
@@ -0,0 +1,4 @@
+import-module servermanager
+add-windowsfeature web-server -includeallsubfeature
+set-content -path "C:\inetpub\wwwroot\Default.html" -Value "This is the server $($env:computername)!"
+#Enable-NetFirewallRule -name FPS-ICMP4-ERQ-In 
